c0e77a61735618e6958e0758e40adb503b96fb58
“CollinABullock” <cbullock@schoolofrock.com>
2023-09-03 16:10:26 -0500

    got the puppies displaying

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import './App.css'
+import './App.css';
+import {useState} from "react";
+import {puppyList} from "./data";
 
 function App() {
-
+const [puppies, setpuppies] = useState(puppyList);
+console.log(puppies);
 
   return (
     <>
       <div>
+        {
+          puppies.map((puppy) => {
+            return <p key={puppy.id}>{puppy.name}</p>
+          })
+          }
         </div>
     </>
   )
