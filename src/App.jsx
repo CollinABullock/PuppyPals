d2ef090a2668967a6e9cbf64007b00fd792741e3
“CollinABullock” <cbullock@schoolofrock.com>
2023-09-03 16:58:30 -0500

    adding functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,32 @@ import {puppyList} from "./data";
 
 function App() {
 const [puppies, setpuppies] = useState(puppyList);
+const [featPupId, setFeatPupId] = useState(null);
 console.log(puppies);
+
+const featuredPup = puppies.find((pup) => pup.id === featPupId);
+console.log(featuredPup);
 
   return (
     <>
       <div>
         {
           puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>
+            return <p onClick={() => {setFeatPupId(puppy.id);}} key={puppy.id}>{puppy.name}</p>
           })
           }
+          
+       {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>{featuredPup.age} years old</li>
+            <li>{featuredPup.email}</li>
+            <li>Puppy id: {featuredPup.id}</li>
+            <li>Owner id: {featuredPup.ownerId}</li>
+          </ul>
+        </div>
+       )}
         </div>
     </>
   )
